0caa7724b574829b698af00d39212fcf1775fea2
Smitesh Patil <smiteshpatil2010@gmail.com>
2021-08-22 07:05:50 +0530

    Add javascript support

--- lua/nvim-gps/init.lua
+++ lua/nvim-gps/init.lua
@@ -81,6 +81,22 @@ local query = {
 			name: (identifier) @method-name
 				body: (block)) @scope-root)
 	]],
+	["javascript"] = [[
+		; Class
+		((class_declaration
+			name: (identifier) @class-name
+			body: (class_body)) @scope-root)
+
+		; Function
+		((function_declaration
+			name: (identifier) @function-name
+			body: (statement_block)) @scope-root)
+
+		; Method
+		((method_definition
+			name: (property_identifier) @method-name
+			body: (statement_block)) @scope-root)
+	]],
 	["lua"] = [[
 		; Function
 		((function
